8cac3bed89a855cc3ea6b48ba392160822f497ae
Mikhail Grachev <work@mgrachev.com>
2023-05-26 16:13:55 +0300

    Get rid of `id`
    Signed-off-by: Mikhail Grachev <work@mgrachev.com>

--- src/v1/agent.rs
+++ src/v1/agent.rs
@@ -6,8 +6,8 @@ use svc_agent::AgentId;
 #[serde(tag = "label", rename_all = "snake_case")]
 #[serde(rename(deserialize = "AgentEvent"))]
 pub enum AgentEventV1 {
-    Enter { id: i64, agent_id: AgentId },
-    Leave { id: i64, agent_id: AgentId },
+    Enter { agent_id: AgentId },
+    Leave { agent_id: AgentId },
 }
 
 impl From<AgentEventV1> for EventV1 {
@@ -29,14 +29,14 @@ mod tests {
         fn serialize_test() {
             let agent_id = AgentId::from_str("instance01.service_name.svc.example.org")
                 .expect("parse agent_id");
-            let agent = AgentEventV1::Enter { id: 1, agent_id };
+            let agent = AgentEventV1::Enter { agent_id };
             let event = EventV1::Agent(agent);
 
             let json = serde_json::to_string(&event).expect("serialization to string");
 
             assert_eq!(
                 json,
-                "{\"entity_type\":\"agent\",\"label\":\"enter\",\"id\":1,\"agent_id\":\"instance01.service_name.svc.example.org\"}"
+                "{\"entity_type\":\"agent\",\"label\":\"enter\",\"agent_id\":\"instance01.service_name.svc.example.org\"}"
             )
         }
 
@@ -47,7 +47,6 @@ mod tests {
                 {
                     "entity_type": "agent",
                     "label": "leave",
-                    "id": 2,
                     "agent_id": agent_id,
                 }
             );
@@ -55,7 +54,7 @@ mod tests {
             let event1 = serde_json::from_str::<EventV1>(&json).unwrap();
 
             let agent_id = AgentId::from_str(agent_id).expect("parse agent_id");
-            let agent = AgentEventV1::Leave { id: 2, agent_id };
+            let agent = AgentEventV1::Leave { agent_id };
             let event2 = EventV1::Agent(agent);
 
             assert_eq!(event1, event2);
